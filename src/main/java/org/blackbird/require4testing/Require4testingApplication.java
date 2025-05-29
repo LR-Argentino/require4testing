dcecca2d7c2b1f0c6b47205e00d3248be0f6dd01
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-05-29 17:23:59 +0200

    refactor: update pom.xml and application properties for improved dependency management and logging

--- src/main/java/org/blackbird/require4testing/Require4testingApplication.java
+++ src/main/java/org/blackbird/require4testing/Require4testingApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Require4testingApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Require4testingApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(Require4testingApplication.class, args);
+  }
 }
